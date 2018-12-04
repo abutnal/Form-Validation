90f3334ff514ac14624f1851b67e7fb9ab12cf61
abutnal <45108373+abutnal@users.noreply.github.com>
2018-12-04 12:35:41 +0530

    Add files via upload
    Ajax From Validation

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+<script type="text/javascript" language="javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="assets/js/script.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" type="image/png" href="images/favicon.ico">
+<title>Form Validation in jQuery</title>
+</head>
+
+<body>
+<br><br>
+<center>
+<h1>The form has been submitted!</h1>
+</center>
+
+
+
+
+</body>
+</html>
